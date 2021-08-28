55486308387f8626e5f8522a371404106fb2ab24
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2021-08-28 11:22:35 +1000

    complete with shuffle function and colors displayed

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
     var r, g, b;
     var array = [];
     var backColor;
-    for (r = 0; r <= 256; r+= 8) {
-      for (g = 0; g <= 256; g+= 8) {
-        for (b = 0; b <= 256; b+= 8) {
+    for (r = 0; r <= 256; r += 8) {
+      for (g = 0; g <= 256; g += 8) {
+        for (b = 0; b <= 256; b += 8) {
           backColor = "rgb(" + r + "," + g + "," + b + ")";
           array.push(backColor);
 
@@ -24,12 +24,63 @@ class App extends React.Component {
     }
   }
 
+  shuffleColors = (array) => {
+    var currIndex = array.length,
+      randomIndex;
+
+    while (currIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currIndex);
+      currIndex--;
+
+      [array[currIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currIndex],
+      ];
+    }
+
+    return array;
+  };
 
   render() {
     var colors = this.state.colors;
-    return <div className="App">
-      {colors.map( color => <p key={color} style={{backgroundColor: color}}> Color {color}</p>)}
-    </div>;
+    var half = Math.ceil(colors.length / 2);
+    var q1 = colors.slice(0, colors.length / 8);
+    var q2 = colors.slice(colors.length / 8, colors.length / 4);
+    var q3 = colors.slice(colors.length / 4, half);
+    var q4 = colors.slice(half, colors.length);
+
+    return (
+      <div className="App">
+        <div>
+          {this.shuffleColors(q1).map((color) => (
+            <p key={color} style={{ backgroundColor: color }}>
+              .
+            </p>
+          ))}
+        </div>
+        <div>
+          {this.shuffleColors(q2).map((color) => (
+            <p key={color} style={{ backgroundColor: color }}>
+              .
+            </p>
+          ))}
+        </div>
+        <div>
+          {this.shuffleColors(q3).map((color) => (
+            <p key={color} style={{ backgroundColor: color }}>
+              .
+            </p>
+          ))}
+        </div>
+        <div>
+          {this.shuffleColors(q4).map((color) => (
+            <p key={color} style={{ backgroundColor: color }}>
+              .
+            </p>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 export default App;
